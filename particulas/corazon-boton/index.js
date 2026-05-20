8fb8657ffd86d3eaa4f3906f0c7e581571641c5f
catharygr <103951447+catharygr@users.noreply.github.com>
2026-05-20 13:32:41 +0200

    Trabajando en el JS

---
+++ particulas/corazon-boton/index.js
@@ -0,0 +1,7 @@
+const particleButton = document.querySelector(".particleButton");
+
+particleButton.addEventListener("click", crearParticulas);
+
+function crearParticulas() {
+  console.log("Funciona");
+}
