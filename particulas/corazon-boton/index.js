0386d5d318e7a18f36cf6c84f275783ea45bce9b
catharygr <103951447+catharygr@users.noreply.github.com>
2026-05-20 14:33:27 +0200

    Trabajando en el JS

--- particulas/corazon-boton/index.js
+++ particulas/corazon-boton/index.js
@@ -1,7 +1,36 @@
 const particleButton = document.querySelector(".particleButton");
 
+function range(inicio, final = false) {
+  const arr = [];
+
+  if (!final) {
+    final = inicio + 1;
+    inicio = 1;
+  }
+
+  for (let i = inicio; i < final; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 particleButton.addEventListener("click", crearParticulas);
 
 function crearParticulas() {
-  console.log("Funciona");
+  const todasParticulas = [];
+  const magnitudes = 48;
+
+  range(5).forEach(() => {
+    const particula = document.createElement("div");
+    particula.classList.add("particula");
+    const x = _.random(-magnitudes, magnitudes);
+    const y = _.random(-magnitudes, magnitudes);
+    particula.style.transform = `translate(${x}px, ${y}px)`;
+    // particula.style.setProperty("--fade-duration", FASE_DURACION + "ms");
+    particleButton.appendChild(particula);
+    todasParticulas.push(particula);
+  });
+
+  window.setTimeout(() => {
+    todasParticulas.forEach((particula) => particula.remove());
+  }, 2000);
 }
