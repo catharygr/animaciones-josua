8f82b74d20877d04eabfbc91c361b15cad1cf34c
catharygr <103951447+catharygr@users.noreply.github.com>
2026-05-29 12:54:05 +0200

    Trabajando en el JS

--- particulas/emoji-lluvia/index.js
+++ particulas/emoji-lluvia/index.js
@@ -1,2 +1,7 @@
 const todosLosEmojis = document.querySelectorAll(".emoji");
 console.log(todosLosEmojis);
+
+todosLosEmojis.forEach((emoji) => {
+  emoji.style.animation = `lluvias ${_.random(2000, 4000)}ms infinite`;
+  emoji.style.animationDelay = `${_.random(2000)}ms`;
+});
